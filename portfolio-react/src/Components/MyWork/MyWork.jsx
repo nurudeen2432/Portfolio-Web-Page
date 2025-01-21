3dda2c72782dd72d6976e02f02921ec8b0e811fb
Nurudeen Durowade <Nurudeen.durowade@wemabank.com>
2025-01-21 20:42:32 +0100

    added first project

--- portfolio-react/src/Components/MyWork/MyWork.jsx
+++ portfolio-react/src/Components/MyWork/MyWork.jsx
@@ -1,26 +1,37 @@
-import React from 'react'
-import './MyWork.css'
-import theme_pattern from '../../assets/theme_pattern.svg'
-import mywork_data from '../../assets/mywork_data'
-import arrow_icon from '../../assets/arrow_icon.svg'
+import React from "react";
+import "./MyWork.css";
+import theme_pattern from "../../assets/theme_pattern.svg";
+import mywork_data from "../../assets/mywork_data";
+import arrow_icon from "../../assets/arrow_icon.svg";
 const MyWork = () => {
   return (
-    <div id='work' className='mywork'>
-        <div className="mywork-title">
-            <h1>My latest work</h1>
-            <img src={theme_pattern} alt="" />
-        </div>
-        <div className="mywork-container">
-            {mywork_data.map((work, index)=>{
-                return <img key={index} src={work.w_img}/>
-            })}
-        </div>
-        <div className="mywork-showmore">
-            <p>Show More</p>
-            <img src={arrow_icon} alt=''/>
-        </div>
+    <div id="work" className="mywork">
+      <div className="mywork-title">
+        <h1>My latest work</h1>
+        <img src={theme_pattern} alt="" />
+      </div>
+      <div className="mywork-container">
+        {mywork_data.map((work, index) => {
+          return (
+            <div key={index} className="work-item">
+              <img
+                key={index}
+                src={work.w_img}
+                onClick={() => (window.open(work.url, '_blank'))}
+                className={`project-img ${work.w_no === 1 ? 'leave-project' : ''}`}
+                alt={work.w_name}
+              />
+              <p className="work-description">{work.desc}</p>
+            </div>
+          );
+        })}
+      </div>
+      <div className="mywork-showmore">
+        <p>Show More</p>
+        <img src={arrow_icon} alt="" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MyWork
+export default MyWork;
--- portfolio-react/src/assets/mywork_data.js
+++ portfolio-react/src/assets/mywork_data.js
@@ -1,4 +1,4 @@
-import project1_img from '../assets/project_1.svg'
+import project1_img from '../assets/leave.svg'
 import project2_img from '../assets/project_2.svg'
 import project3_img from '../assets/project_3.svg'
 import project4_img from '../assets/project_4.svg'
@@ -9,32 +9,44 @@ const mywork_data = [
     {
         w_no:1,
         w_name:"Web design",
-        w_img:project1_img
+        w_img:project1_img,
+        url: "https://leavemanagement-dev.azurewebsites.net/",
+        desc: "Modern Leave Management Portal built with ASP.NET Core MVC. Demo credentials - Email: babatee@gmail.com Password: Test@123"
     },
     {
         w_no:2,
         w_name:"Web design",
-        w_img:project2_img
+        w_img:project2_img,
+        url:"",
+        desc: ""
     },
     {
         w_no:3,
         w_name:"Web design",
-        w_img:project3_img
+        w_img:project3_img,
+        url:"",
+        desc: ""
     },
     {
         w_no:4,
         w_name:"Web design",
-        w_img:project4_img
+        w_img:project4_img,
+        url:"",
+        desc: ""
     },
     {
         w_no:5,
         w_name:"Web design",
-        w_img:project5_img
+        w_img:project5_img,
+        url:"",
+        desc: ""
     },
     {
         w_no:6,
         w_name:"Web design",
-        w_img:project6_img
+        w_img:project6_img,
+        url:"",
+        desc: ""
     },
 ]
  
